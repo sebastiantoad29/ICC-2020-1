ec56cc6ccfe5e8b1fdfb646324ff12a85cf632d0
sebastiantoad29 <53270020+sebastiantoad29@users.noreply.github.com>
2019-08-19 19:21:12 -0500

    Add files via upload

---
+++ Practica01/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+  public static void main(String[] args) {
+    System.out.println("java version 1.8.0_221");
+    System.out.println("Java(TM) SE Runtime Environment (build 1.8.0_221-b11)");
+    System.out.println("Java HotSpot(TM) 64-Bit Server VM (build 25.221-b11, mixed mode)");
+  }
+}
